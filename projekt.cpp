50a7f93a4d57e4a365b71c119cc16fd2059f1317
FranciszekKaniewski <fkaniewski.web@gmail.com>
2024-12-11 19:14:18 +0100

    Projekt.cpp added

---
+++ projekt.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+    int value;
+    std::cout << "Podaj liczbę całkowitą: ";
+    std::cin >> value;
+    std::cout << "Wprowadzona liczba to: " << value << std::endl;
+    
+    return 0;
+}
