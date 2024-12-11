17d19fbbacf8d9c0ff27233bed3acc42ea07f21a
FranciszekKaniewski <fkaniewski.web@gmail.com>
2024-12-11 19:18:13 +0100

    Check if value is greater than 0 added

--- projekt.cpp
+++ projekt.cpp
@@ -4,6 +4,11 @@ int main() {
     int value;
     std::cout << "Podaj liczbę całkowitą: ";
     std::cin >> value;
+    if(value > 0){
+        std::cout << "Wprowadzona liczba jest dodatnia" << std::endl;
+        
+        return 0;
+    }
     std::cout << "Wprowadzona liczba to: " << value << std::endl;
     
     return 0;
