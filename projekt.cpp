029fb2517cdf1acfe9109ce23f7607b9be083a08
FranciszekKaniewski <fkaniewski.web@gmail.com>
2024-12-11 19:15:02 +0100

    Is primary fn added to projekt.cpp

--- projekt.cpp
+++ projekt.cpp
@@ -1,10 +1,21 @@
 #include <iostream>
 
+bool czy_pierwsza(int liczba) {
+    if (liczba <= 1) return false;
+    for (int i = 2; i * i <= liczba; ++i) {
+        if (liczba % i == 0) return false;
+    }
+    return true;
+}
+
 int main() {
     int value;
     std::cout << "Podaj liczbę całkowitą: ";
     std::cin >> value;
     std::cout << "Wprowadzona liczba to: " << value << std::endl;
-    
+
+    czy_pierwsza(value) ? std::cout << "Liczba " << value << " jest liczbą pierwszą." << std::endl :
+        std::cout << "Liczba " << value << " nie jest liczbą pierwszą." << std::endl;
+
     return 0;
 }
